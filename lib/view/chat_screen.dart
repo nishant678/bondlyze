a80784d11772fb1587115afb966bbe2209cc55d6
nishantGurutu <this.is.nishantkumar1@gmail.com>
2025-11-01 00:10:19 +0530

    new changes in chat screen

--- lib/view/chat_screen.dart
+++ lib/view/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:bondlyze/model/chat_item_model.dart';
+import 'package:bondlyze/view/widget/chat_tile.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -8,15 +10,65 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  
+  final List<ChatItem> _chats = const [
+    ChatItem(
+      name: 'Charlotte',
+      message: "Haha, yes I've seen your profil...",
+      timeLabel: '09:41',
+      unreadCount: 1,
+    ),
+    ChatItem(
+      name: 'Aurora',
+      message: 'Wow, this is really epic 👍',
+      timeLabel: '08:54',
+      unreadCount: 3,
+    ),
+    ChatItem(
+      name: 'Victoria',
+      message: 'Thank you so much andrew 🔥',
+      timeLabel: '01:27',
+    ),
+    ChatItem(
+      name: 'Emilia',
+      message: 'Wow love it! ❤️',
+      timeLabel: 'Yesterday',
+      unreadCount: 2,
+    ),
+    ChatItem(
+      name: 'Natalie',
+      message: "I know... I'm trying to get the ...",
+      timeLabel: 'Yesterday',
+    ),
+    ChatItem(
+      name: 'Scarlett',
+      message: "It's strong not just fabulous!😁",
+      timeLabel: 'Dec 20, 2023',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Chat Screen',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
+      appBar: AppBar(
+        title:   Text('Chats'),
+        centerTitle: true,
+
       ),
+      body:  Expanded(
+            child: ListView.separated(
+              itemCount: _chats.length,
+              separatorBuilder: (_, __) => SizedBox(),
+              itemBuilder: (context, index) {
+                final chat = _chats[index];
+                return ChatTile(
+                  chat: chat,
+                  badgeColor: colorScheme.primary,
+                );
+              },
+            ),
+          ),
     );
   }
 }
